fe4852401b7e18523945f9ed949017642bfee315
zsumore <zsumore@163.com>
2016-05-06 09:23:46 +0800

    haha2

--- src/main/java/com/fs121/monitor/Application.java
+++ src/main/java/com/fs121/monitor/Application.java
@@ -27,15 +27,4 @@ public class Application extends SpringBootServletInitializer {
 		SpringApplication.run(Application.class, args);
 	}
 
-	// @Bean
-	// public WebMvcConfigurer corsConfigurer() {
-	// return new WebMvcConfigurerAdapter() {
-	// @Override
-	// public void addCorsMappings(CorsRegistry registry) {
-	// //registry.addMapping("/AtmosphericElectric/**");
-	// System.out.println(registry.toString());
-	// }
-	// };
-	// }
-
 }
